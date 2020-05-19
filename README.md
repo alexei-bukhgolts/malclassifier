513603a6e82a2193c26f90f757b74fd80998a36b
altg0x0 <altg0x0@yandex.ru>
2020-05-19 23:33:57 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # malclassifier
-A test program that should classify malware. Useless.
+A test program that should classify malware.
+
+You need to have behavior.csv in this directory.
+Have keras, scikit-learn, eli5, pandas, numpy installed.
+
+Run main.py to test predictions and print predicted and actual data as two arrays.
+
+Run findfeatures.py IN JUPYTER NOTEBOOK to find the most important features.
+
+features.html is file containing some of the most important features.
---
+++ findfeatures.py
@@ -0,0 +1,38 @@
+""" Simple linear topology neural network """
+import pandas
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+import eli5
+from eli5.sklearn import PermutationImportance
+import numpy as np
+
+FINAL_TEST_ROWS = 3000
+
+df = pandas.read_csv("behavior.csv", usecols=lambda x: x != '')
+isMalware = df['tag'].values
+del df['tag']  # We should not use the answer as input data
+df.drop(df.columns[0], axis=1, inplace=True)  # 0th column is some kind of hash
+xLen = len(df.columns)
+df.reindex(np.random.permutation(df.index))
+inputs = df.values
+
+X = inputs.astype(float)
+Y = isMalware
+
+
+def createModel():
+    model = Sequential()
+    model.add(Dense(xLen, input_dim=xLen, activation='relu'))
+    model.add(Dense(xLen // 2, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+classifier = KerasClassifier(build_fn=createModel, epochs=30, batch_size=32, verbose=2, validation_split=0.2)
+classifier.fit(X[FINAL_TEST_ROWS:], Y[FINAL_TEST_ROWS:])
+
+perm = PermutationImportance(classifier, random_state=1).fit(X[:FINAL_TEST_ROWS], Y[:FINAL_TEST_ROWS])
+eli5.show_weights(perm, feature_names=df.columns.tolist())
+
